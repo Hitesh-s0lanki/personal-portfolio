1b859a43af21085803390b8c5d323c133aa434e3
hitesh-s0lanki <hiteshsolanki4623@gmail.com>
2025-04-22 20:06:23 +0530

    first

---
+++ src/components/home/home-description.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+const HomeDescription = () => {
+  return (
+    <div className="w-full bg-[linear-gradient(180deg,#2F3D59,#0B1326)] shadow-2xl drop-shadow-2xl rounded-4xl flex flex-col gap-5 justify-center items-center p-5 py-10 mt-[150px]">
+      <div
+        className="
+    w-[200px] h-[200px]
+   bg-[linear-gradient(180deg,#142131,#000113)]
+     rounded-4xl  absolute -top-20 z-1
+  "
+      >
+        {/* Make this cricle lighter */}
+
+        <div
+          className="
+                w-full h-full rounded-4xl
+                bg-[radial-gradient(circle_at_top_left,rgba(242,84,91,0.25),transparent_60%)]
+                p-3
+              "
+        >
+          <Image
+            src="/profile.png"
+            alt="Hitesh Solanki"
+            width={200}
+            height={200}
+            className=" rounded-4xl "
+          />
+        </div>
+      </div>
+      <div className="h-[80px]"></div>
+      <div className=" text-center">
+        <h1 className=" text-2xl text-white font-semibold">Hitesh Solanki</h1>
+        <h3 className="text-sm text-muted-foreground">@hitesh-s0lanki</h3>
+      </div>
+      <div className="flex flex-col gap-2 text-white text-sm text-start">
+        <p>🎈04.06.2003</p>
+        <p>📍Mumbai, India</p>
+        <p>💻Software Engineer | Builder</p>
+      </div>
+    </div>
+  );
+};
+
+export default HomeDescription;
---
+++ src/components/home/social-cards.tsx
@@ -0,0 +1,62 @@
+import { ExternalLink, Github, Mail } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const SocialCard = () => {
+  return (
+    <div className="w-full flex flex-col gap-5">
+      <div className="grid grid-cols-3 gap-4">
+        <div className=" col-span-2 w-full p-4 bg-[#143061] rounded-2xl opacity-80 space-y-4">
+          <div className=" w-full flex justify-between items-start">
+            <Mail className="text-white size-10" />
+            <Link href="mailto:hiteshsolanki4623@gmail.com">
+              <ExternalLink className="text-white size-5" />
+            </Link>
+          </div>
+          <div className="flex flex-col gap-1 w-full">
+            <h1 className="text-white text-sm font-semibold">Email</h1>
+            <p className="text-white text-xs flex flex-wrap w-full text-wrap">
+              hiteshsolanki4623@gmail.com
+            </p>
+          </div>
+        </div>
+        <div className="w-full p-4 bg-[linear-gradient(180deg,#35A5EE,#1264D6)] rounded-2xl opacity-80 space-y-4">
+          <div className=" w-full flex justify-between items-start">
+            <Image
+              src="/social/leetcode.png"
+              alt="leetcode"
+              height={50}
+              width={50}
+              className="text-white size-10"
+            />
+            <Link href="https://github.com/Hitesh-s0lanki" target="_blank">
+              <ExternalLink className="text-white size-5" />
+            </Link>
+          </div>
+          <div className="flex flex-col gap-1 w-full">
+            <h1 className="text-white text-sm font-semibold">Leetcode</h1>
+            <p className="text-white text-xs flex flex-wrap w-full text-wrap">
+              @hitesh4623
+            </p>
+          </div>
+        </div>
+        <div className=" col-span-3 w-full p-4 bg-[linear-gradient(180deg,#25262F,#0C0D16)] rounded-2xl opacity-80 space-y-4">
+          <div className=" w-full flex justify-between items-start">
+            <Github className="text-white size-10" />
+            <Link href="https://github.com/Hitesh-s0lanki" target="_blank">
+              <ExternalLink className="text-white size-5" />
+            </Link>
+          </div>
+          <div className="flex flex-col gap-1 w-full">
+            <h1 className="text-white text-sm font-semibold">Github</h1>
+            <p className="text-white text-xs flex flex-wrap w-full text-wrap">
+              @hitesh-s0lanki
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SocialCard;
